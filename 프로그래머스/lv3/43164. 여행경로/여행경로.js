a2cbe2a70afaeb5866f2a895565eb119ddb29cb2
SEO <emma05127@gmail.com>
2023-04-09 22:16:45 +0900

    [level 3] Title: 여행경로, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43164. 여행경로/여행경로.js
@@ -0,0 +1,26 @@
+function solution(tickets) {
+    const answer = [];
+    const goal = tickets.length + 1;
+    const chk = Array.from({length: tickets.length}, _ => 0);
+    
+    const dfs = (path) => {
+        if (path.length === goal) {
+            answer.push(path)
+        } else {
+            for(const i in tickets) {
+                if(chk[i] === 0) {
+                    const [start, end] = tickets[i];
+                    if (path[path.length - 1] === start) {
+                        chk[i] = 1;
+                        dfs([...path, end]);
+                        chk[i] = 0;
+                    }
+                }
+            }
+        }
+    }
+    
+    dfs(["ICN"]);
+    
+    return answer.sort()[0];
+}
