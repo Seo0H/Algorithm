e45342dbec32197336574d4e79b853f28a282ae4
SEO <emma05127@gmail.com>
2023-04-10 18:48:40 +0900

    [level 3] Title: 여행경로, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv3/43164. 여행경로/여행경로.js
+++ 프로그래머스/lv3/43164. 여행경로/여행경로.js
@@ -9,17 +9,20 @@ function solution(tickets) {
         if (path.length === goal) answer.push(path)
         else {
             for(const i in tickets) {
+                // 방문하지 않았다면
                 if(chk[i] === 0) {
                     const [start, end] = tickets[i];
+                    // 마지막으로 방문한 곳이 이번 티켓 시작과 같다면
                     if (path[path.length - 1] === start) {
+                        // 방문
                         chk[i] = 1;
+                        // 다음 위치로 이동.
                         dfs([...path, end]);
+                        // 재귀를 탈출했을때 -> 해당 깊이에서 답을 찾지 못했을때 -> 원상복귀.
                         chk[i] = 0;
                     }
                 }
             }
         }
     }
-    
-    
 }
