c1e4f0afd11bee264a138b01166c852b3c10ac6b
SEO <emma05127@gmail.com>
2023-04-12 15:13:13 +0900

    [level 3] Title: 단어 변환, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv3/43163. 단어 변환/단어 변환.js
+++ 프로그래머스/lv3/43163. 단어 변환/단어 변환.js
@@ -1,20 +1,17 @@
-// 0409 두번째 풀이
 function solution(begin, target, words) {
-    if(!words.includes(target)) return 0;
+    if (!words.includes(target)) return 0;
+    const q = [[begin, 0]];
     
-    let answer = 0;
-    const q = [[begin, 0]]; // [비교할 단어, 카운트 횟수]
-    
-    while(q.length > 0){
-        const [compare, count] = q.shift();
-        if(compare === target) return count;
-        words.forEach((word,idx) =>{
-            let isCharDiffrent = 1;
-            for(let i=0; i<compare.length; i++) if(compare[i] !== word[i]) isCharDiffrent -= 1;
-            if(isCharDiffrent === 0){
-                q.push([word, count+1]);
+    while(q.length){
+        const [compareTxt, count] = q.shift();
+        if (compareTxt === target) return count;
+        words.forEach((e,idx) => {
+            let isDiff = 1;
+            for(let i=0; i<e.length ; i++) if(e[i] !== compareTxt[i]) isDiff -= 1;
+            if (isDiff === 0){
                 words[idx] = '';
-            }
+                q.push([e, count+1]);
+            } 
         })
     }
     
