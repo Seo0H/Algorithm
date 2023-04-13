351f61efc55500fa95401fe74dc994da5161dd6b
SEO <emma05127@gmail.com>
2023-04-13 22:06:29 +0900

    [level 3] Title: 여행경로, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv3/43164. 여행경로/여행경로.js
+++ 프로그래머스/lv3/43164. 여행경로/여행경로.js
@@ -1,28 +1,23 @@
 function solution(tickets) {
-    const answer = [];
-    const goal = tickets.length + 1;
-    const chk = Array.from({length: tickets.length}, _ => 0);
-    dfs(["ICN"]);
+    const answer = []
+    const visitied = Array.from({length : tickets.length}, _ => false)
+    bfs(['ICN']);
     return answer.sort()[0];
     
-    function dfs (path){
-        if (path.length === goal) answer.push(path)
-        else {
-            for(const i in tickets) {
-                // 방문하지 않았다면
-                if(chk[i] === 0) {
-                    const [start, end] = tickets[i];
-                    // 마지막으로 방문한 곳이 이번 티켓 시작과 같다면
-                    if (path[path.length - 1] === start) {
-                        // 방문
-                        chk[i] = 1;
-                        // 다음 위치로 이동.
-                        dfs([...path, end]);
-                        // 재귀를 탈출했을때 -> 해당 깊이에서 답을 찾지 못했을때 -> 원상복귀.
-                        chk[i] = 0;
-                    }
-                }
-            }
+    function bfs(path){
+    if (path.length === tickets.length + 1) {
+        answer.push(path);
+    } else {
+        tickets.forEach((e,idx) => {
+            const [departure, arrival] = e;
+            if(!visitied[idx] && departure === path[path.length - 1]) {
+                visitied[idx] = true;
+                bfs([...path, arrival])
+                visitied[idx] = false;
+                };
+            })
         }
     }
 }
+
+
