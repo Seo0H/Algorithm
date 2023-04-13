13c3bfbbf08e464c91d2bc4c5b14622de7365888
SEO <emma05127@gmail.com>
2023-04-13 22:27:52 +0900

    [level 2] Title: 타겟 넘버, Time: 1.82 ms, Memory: 35.6 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.js
@@ -3,15 +3,14 @@ function solution(numbers, target) {
     dfs(0,0);
     return answer;
     
-    function dfs(index, sum){
-        //index === numbers.length : 주어진 숫자를 다 더했다는 뜻.
-        // 이 상태에서 판단해야 함
-        if(index === numbers.length) {
+    function dfs(idx,sum){
+        if(idx === numbers.length){
             if(sum === target) answer++;
             return;
         }
         
-        dfs(index+1, sum + numbers[index]);
-        dfs(index+1, sum - numbers[index]);
-    }
+        dfs(idx+1,sum + numbers[idx]);
+        dfs(idx+1,sum - numbers[idx]);
+    }    
 }
+
