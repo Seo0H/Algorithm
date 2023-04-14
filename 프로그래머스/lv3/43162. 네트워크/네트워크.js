f64684e0b2bb30c4ead43b684d75f9b33bc72e94
SEO <emma05127@gmail.com>
2023-04-14 17:39:42 +0900

    [level 3] Title: 네트워크, Time: 1.76 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.js
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.js
@@ -1,29 +1,24 @@
 function solution(n, computers) {
     var answer = 0;
     
-    for(let y=0; y<n; y++){
-        for(let x=0; x<n; x++){
-            if(computers[y][x]===1) dfs(y,x);
-        }
+    for(let y=0; y<computers.length; y++){
+       for(let x=0; x<computers.length; x++){
+            if(computers[y][x] === 1) {
+                answer++;
+                computers[y][x] = -1;
+                dfs(x)
+            }
+        } 
     }
-    
     return answer;
     
-    function dfs(y,x){
-        answer++;
-        computers[y][x] = -1;
-        
-        const q=[y];
-        
-        while(q.length > 0){
-            const qy = q.shift();
-            for(let i=0; i<n; i++){
-                if(computers[qy] && computers[qy][i] === 1){
-                    computers[qy][i] = -1;
-                    q.push(i);
-                }
-            }
-        }
-    }    
+    function dfs(y){
+        for(let i=0; i<computers.length; i++){
+           if(computers[y][i] === 1) {
+               computers[y][i] = -1;
+               dfs(i);
+           }
+       }
+    }
+    
 }
-
