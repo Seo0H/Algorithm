2c35fe57d2cf03b88648f89b725d6191311641ab
SEO <emma05127@gmail.com>
2023-04-17 16:58:22 +0900

    [level 3] Title: 아이템 줍기, Time: 1.43 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv3/87694. 아이템 줍기/아이템 줍기.js
+++ 프로그래머스/lv3/87694. 아이템 줍기/아이템 줍기.js
@@ -1,38 +1,33 @@
 function solution(rectangle, characterX, characterY, itemX, itemY) {
-    var answer = 0;
-    const characterX2 = characterX * 2;
-    const characterY2 = characterY * 2;
-    
-    const filledMapx2 = rectangle.reduce((acc, [a, b, c, d]) => {
-        const minX = a*2, minY = b*2, maxX = c*2, maxY = d*2;
-        for(let y = minY ; y<=maxY; y++)
-            for(let x = minX ; x<=maxX; x++) acc[y][x] = 1;
+    const rectangleX2 = rectangle.reduce((acc,[a, b, c, d]) => {
+        const minX =a*2, minY=b*2, maxX=c*2, maxY=d*2;
+        for(let y=minY; y<=maxY; y++){
+            for(let x=minX; x<=maxX; x++) acc[y][x] = 1;
+        }
         return acc;
-    }, Array.from({length: 102}, _=> Array.from({length: 102}, _=>0)))
+    }, Array.from({length: 102}, _=> Array.from({length: 102},_=> 0)))
     
-    const outlineMapx2 = rectangle.reduce((acc, [a, b, c, d] ) => {
-        const minX = a*2, minY = b*2, maxX = c*2, maxY = d*2;
-        for(let y = minY+1 ; y<maxY; y++)
-            for(let x = minX+1 ; x<maxX; x++) acc[y][x] = 0;
+    const outLineMap = rectangle.reduce((acc,[a, b, c, d]) => {
+        const minX =a*2, minY=b*2, maxX=c*2, maxY=d*2;
+        for(let y=minY+1; y<maxY; y++){
+            for(let x=minX+1; x<maxX; x++) acc[y][x] = 0;
+        }
         return acc;
-    }, filledMapx2)
+    }, rectangleX2)
     
-    const q = [[characterY2,characterX2,0]];
+    const q = [[characterY*2, characterX*2, 0]];
     const dir = [[0,1],[0,-1],[1,0],[-1,0]];
     
     while(q.length){
         const [qy, qx, cnt] = q.shift();
         if(qy === itemY*2 && qx === itemX*2) return cnt/2;
-        for(let d of dir){
-            const [dy, dx] = d;
+        for(let [dy,dx] of dir){
             const y = qy+dy;
             const x = qx+dx;
-            if(outlineMapx2[y] && outlineMapx2[y][x] === 1){
+            if(outLineMap[y] && outLineMap[y][x] === 1 ){
                 q.push([y,x,cnt+1]);
-                outlineMapx2[y][x] = -1;
+                outLineMap[y][x] = -1;
             }
         }
-        
     }
-    return -1;
 }
