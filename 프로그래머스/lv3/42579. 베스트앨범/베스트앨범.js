988de31df9c81ffced48f6333d2ca48e1f414f65
SEO <emma05127@gmail.com>
2023-04-17 17:30:12 +0900

    [level 3] Title: 베스트앨범, Time: 0.20 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.js
+++ 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.js
@@ -4,40 +4,21 @@ function solution(genres, plays) {
         return memo;
     },{})
 
-    const singsCount = genres.reduce((memo, cur, idx) => {
-        if (!memo[cur]) memo[cur] = {_1st : idx };
-        else if (!memo[cur]._2nd) {
-            if(plays[memo[cur]._1st] < plays[idx]){
-                memo[cur]._2nd = memo[cur]._1st;
-                memo[cur]._1st = idx;
-            }
-            else memo[cur]._2nd = idx;
-        }
-        else {
-            const { _1st, _2nd } = memo[cur];
-            // 이번 곡의 재생 횟수가, _1st곡의 재생 횟수와 같은 경우.
-            if(plays[idx] === plays[_1st] ) memo[cur]._2nd = idx;
-            // 이번 곡의 재생 횟수가, 기록된 _1st곡의 재생 횟수보다 많은 경우
-            else if(plays[idx] > plays[_1st]) {
-                memo[cur]._2nd = _1st;
-                memo[cur]._1st = idx;
-            }
-            // 이번 곡의 재생 횟수가, _2st곡의 재생 횟수와 같은 경우.
-            else if(plays[idx] === plays[_2nd] ) memo[cur]._2nd = Math.min(_2nd, idx);
-            // 이번 곡의 재생 횟수가, 기록된 _1st곡의 재생 횟수보다는 적지만, _2nd곡보단 많은 경우
-            else if(plays[idx] > plays[_2nd]) memo[cur]._2nd = idx;
-
-        } 
-         return memo;
-    },{})
-    
-    const descGenres = Object.entries(genresCount).sort((a,b) => b[1] - a[1]);
-
-    let answer = [];
-    descGenres.forEach((e,idx) => {
-        const [key] = e;
-        answer.push(...Object.values(singsCount[key]))
-        })
-    
-    return answer;
+    var dupDic = {};    
+    return genres.map((t,i)=> ({genre : t, count:plays[i] , index:i}))
+        .sort((a,b)=>{
+        // 장르가 다르고 재생횟수가 큰 경우 내림차순 정렬
+        if(a.genre !== b.genre) return genresCount[b.genre] - genresCount[a.genre];
+        // 장르가 같고 재생횟수가 큰 경우 내림차순 정렬
+        if(a.count !== b.count) return b.count - a.count;
+        // 장르가 같고 재생횟수가 같은 경우 인덱스를 기준으로 오름차순 정렬
+               return a.index - b.index;
+           })
+        .filter(t =>  {
+        // 상위 2개만 카운트
+        if(dupDic[t.genre] >= 2) return false;
+        dupDic[t.genre] = dupDic[t.genre] ? dupDic[t.genre]+ 1 : 1;
+        return true;
+            })
+        .map(t=> t.index); 
 }
