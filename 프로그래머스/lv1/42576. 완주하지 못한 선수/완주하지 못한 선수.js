df2ae7afc3494277dc2729722f0dca393daa112a
SEO <emma05127@gmail.com>
2023-04-19 15:08:13 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 51.20 ms, Memory: 68.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,11 +1,13 @@
-// 0413
+// 0419 3번째
 function solution(participant, completion) {
-    const completerMemo = completion.reduce((memo, cur) =>{
+    const completionCount = completion.reduce((memo,cur) => {
         memo[cur] ? memo[cur]++ : memo[cur] = 1;
         return memo
     },{})
+    
     for(let i=0; i<participant.length; i++){
-        if(!completerMemo[participant[i]]) return participant[i];
-        completerMemo[participant[i]]--;
+        if(completionCount[participant[i]] > 0) completionCount[participant[i]]-- 
+        else return participant[i]
     }
+    
 }
