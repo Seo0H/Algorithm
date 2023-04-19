b1c6aacb62293f8a035ad43eaf358e0425a00f90
SEO <emma05127@gmail.com>
2023-04-19 15:17:19 +0900

    [level 1] Title: 모의고사, Time: 1.77 ms, Memory: 34 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.js
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.js
@@ -1,27 +1,18 @@
 function solution(answers) {
-    const no1 = [1, 2, 3, 4, 5];
-    const no2 = [2, 1, 2, 3, 2, 4, 2, 5];
-    const no3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5,];
+    const answer =[];
+    const _1 = [1, 2, 3, 4, 5];
+    const _2 = [2, 1, 2, 3, 2, 4, 2, 5];
+    const _3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
     
-    let no1Idx = 0, no2Idx = 0, no3Idx = 0;
+    const _1grade = answers.filter((answer,idx) => _1[idx%_1.length] === answer).length;
+    const _2grade = answers.filter((answer,idx) => _2[idx%_2.length] === answer).length;
+    const _3grade = answers.filter((answer,idx) => _3[idx%_3.length] === answer).length;
+    const max = Math.max(_1grade, _2grade, _3grade );
     
-    let grading = answers.reduce((acc, cur, idx)=>{
-        if (no1Idx >= no1.length) no1Idx = 0;
-        if (no2Idx >= no2.length) no2Idx = 0;
-        if (no3Idx >= no3.length) no3Idx = 0;
-        
-        no1[no1Idx] === cur ? acc[1]++ : acc;
-        no2[no2Idx] === cur ? acc[2]++ : acc;
-        no3[no3Idx] === cur ? acc[3]++ : acc;
-        
-        no1Idx++; no2Idx++; no3Idx++;
-        return acc;
-    }, {1:0, 2:0, 3:0});
+    if(max === _1grade) answer.push(1);
+    if(max === _2grade) answer.push(2);
+    if(max === _3grade) answer.push(3);
     
-    const tmp = Object.entries(grading)
-            .sort((a,b) => b[1] - a[1])
-            .filter(([i, grad], _, arr) => arr[0][1] === grad )
-            .map (([idx]) => +idx)
-    
-    return tmp;
+    return answer;
+
 }
