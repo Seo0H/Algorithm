2fd728992605a99c26ad6b1b3427fe2114667c15
SEO <emma05127@gmail.com>
2023-04-20 17:54:23 +0900

    [level 2] Title: 위장, Time: 0.20 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 위장/위장.js
+++ 프로그래머스/lv2/42578. 위장/위장.js
@@ -1,6 +1,8 @@
-function solution(clothes) {    
-    return Object.values(clothes.reduce((acc,[_,kind]) => {
-        acc[kind] ? acc[kind]++ : acc[kind]=1;
+function solution(clothes) {
+    const kind = Object.values(clothes.reduce((acc,[_, kind]) => {
+        acc[kind] ? acc[kind]++ : acc[kind] = 1;
         return acc
-    },{})).reduce((acc,cur) => acc*(cur+1),1)-1;
+    } , {}))
+    
+    return kind.reduce((acc,cur) => acc*=(cur+1),1)-1;
 }
