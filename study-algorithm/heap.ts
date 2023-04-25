4803a9f579a4083cdb70eb4472a48390e441ae6a
DESKTOP-1V9OQ1L\GIGABYTE <emma05127@gmail.com>
2023-04-25 16:51:15 +0900

    Study: Heap & PriorityQeue

--- study-algorithm/heap.ts
+++ study-algorithm/heap.ts
@@ -1,28 +1,49 @@
-class MinHeap {
+interface HeapImpl {
+  heap: { key: number; value: any }[];
+  insert(key: number, value: number): void;
+  remove(): void;
+}
+
+interface PriorityQeueImpl extends Heap {
+  enqueue(priority: number, value: any): void;
+  dequeue(): void;
+  isEmpty(): boolean;
+}
+export class Heap implements HeapImpl {
   heap: { key: number; value: number }[];
   constructor() {
     // 힙을 저장할 배열
     this.heap = [];
   }
-
   #getLeftChildIndex = (parentIndex: number) => parentIndex * 2 + 1;
   #getRightChildIndex = (parentIndex: number) => parentIndex * 2 + 2;
   #getParentIndex = (childIndex: number) => Math.floor((childIndex - 1) / 2);
-  #peek = () => this.heap[0]; // 항상 최상위 노드가 peek 됨.
+  peek = () => this.heap[0]; // 항상 최상위 노드가 peek 됨.
 
-  insert = (key: number, value: number) => {
+  insert = (key: number, value: number = key) => {
     // 1. 배열에 끝에 넣는다
     // 2. Min Heap 의 형태를 갖추도록 조절 -> 최근에 삽입한 노드가 제 자리를 찾아갈 수 있도록 끌어올리기
     // 우선순위를 비교하기 위해서 key, value 로 받는다.
     const node: { key: number; value: number } = { key, value }; // 객체로 node 를 만들고
     this.heap.push(node); // push 한다.
-    this.heapifyUp(); // 배열에 가장 끝에 넣고, 다시 min heap 의 형태를 갖추도록 한다.
+    this.#heapifyUp(); // 배열에 가장 끝에 넣고, 다시 min heap 의 형태를 갖추도록 한다.
   };
 
-  heapifyUp = () => {
+  remove = () => {
+    const count = this.heap.length;
+    const rootNode = this.heap[0];
+
+    if (count < 0) return undefined;
+    if (count === 1) this.heap = [];
+    else {
+      this.heap[0] = this.heap.pop() as { key: number; value: number };
+      this.#heapifDown();
+    }
+  };
+
+  #heapifyUp = () => {
     let index = this.heap.length - 1;
     const lastInserteNode = this.heap[index];
-
     while (index > 0) {
       const parentIndex = this.#getParentIndex(index);
       // 부모 노드의 key 값이 마지막에 삽입된 노드의 키 값보다 크면
@@ -36,14 +57,40 @@ class MinHeap {
     // 마지막에 찾아진 곳이 가장 나중에 들어온 노드가 들어갈 자리
     this.heap[index] = lastInserteNode;
   };
+
+  #heapifDown = () => {
+    let index = 0;
+    const count = this.heap.length;
+    const rootNode = this.heap[0];
+
+    while (this.#getLeftChildIndex(index) < count) {
+      const leftChildIndex = this.#getLeftChildIndex(index);
+      const rightChildIndex = this.#getRightChildIndex(index);
+
+      // 왼쪽 오른쪽 중 더 작은 노드를 찾는다
+      // rightChild가 있다면 key값을 비교해 더 작은 값을 찾는다
+      // 없다면 leftChild가 더 작은 값을 가지는 인덱스가 된다
+      const smallerChildIndex =
+        rightChildIndex < count &&
+        this.heap[rightChildIndex].key < this.heap[leftChildIndex].key
+          ? rightChildIndex
+          : leftChildIndex;
+      // 자식 노드의 키 값이 루트노드보다 작다면 위로 끌어올린다.
+      if (this.heap[smallerChildIndex].key <= rootNode.key) {
+        this.heap[index] = this.heap[smallerChildIndex];
+        index = smallerChildIndex;
+      } else break;
+    }
+    this.heap[index] = rootNode;
+  };
 }
 
-const myHeap = new MinHeap();
-myHeap.insert(0, 0);
-console.log(myHeap.heap);
-myHeap.insert(5, 5);
-myHeap.insert(1, 1);
-myHeap.insert(7, 7);
-myHeap.insert(10, 10);
-myHeap.insert(3, 3);
-console.log(myHeap.heap);
+export class PriorityQeue extends Heap implements PriorityQeueImpl {
+  constructor() {
+    super();
+  }
+  enqueue = (priority: number, value: any = priority) =>
+    this.insert(priority, value);
+  dequeue = () => this.remove();
+  isEmpty = () => this.heap.length <= 0;
+}
