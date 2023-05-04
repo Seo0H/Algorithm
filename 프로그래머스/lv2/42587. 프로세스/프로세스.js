65fa477b5f1639b3df607e43a8b075eaa4e8ba2e
SEO <emma05127@gmail.com>
2023-05-04 09:51:19 +0900

    [level 2] Title: 프로세스, Time: 0.15 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42587. 프로세스/프로세스.js
+++ 프로그래머스/lv2/42587. 프로세스/프로세스.js
@@ -1,26 +1,23 @@
 function solution(q, location) {
-    var answer = 0;
+    let answer = 0;
+    
     while(true){
-        
         const process = q.shift();
         const beforeLen = q.length;
         
         for(let i=0 ; i<q.length; i++){
             if(q[i] > process) {
                 q.push(process);
-                
                 break;
             }
         }
+        
         const afterLen = q.length;
         if(beforeLen === afterLen) {
             answer++;
             if(location === 0) break;
         }
-        
         location = location === 0 ? q.length-1 : location-1;
-        
-        
     }
     
     return answer;
