0682295c377cca4544868a5b5ac9d352d74ed7a7
SEO <emma05127@gmail.com>
2023-06-22 13:51:53 +0900

    [unrated] Title: 추억 점수, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.js
@@ -0,0 +1,22 @@
+function solution(name, yearning, photos) {
+    var answer = [];
+    photos.forEach((photo) => {
+        answer.push(photo.reduce((sumYearning,thisName,idx)=> {
+            const hasThisNameIdx = name.indexOf(thisName);
+            if(hasThisNameIdx >= 0) sumYearning += yearning[hasThisNameIdx];
+            return sumYearning;
+        },0))
+    })
+    return answer;
+}
+
+// name과 yarning의 idx는 일치한다.
+// photo[i] 의 배열의 길이는 3 ~ 7사이다
+// photo[i] 의 원소는 중복되지 않는다.
+
+// 1. photo를 순차적으로 순회를 시작한다.
+// 2. yarning을 더하는 변수인 sumYarning을 선언한다.
+// 3. photo[i] 의 요소를 하나씩 순회한다.
+// 4. photo[i][d] 의 요소가 name 있을 경우 해당 idx를 구한다.
+// 4-1. yearning[idx] 를 sumYarning에 더한다.
+// 5. photo[i]의 순회할 요소가 더이상 없을때 sumYarning을 answer에 push한다.
