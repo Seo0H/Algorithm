ea47a8816bf3a01f70e101c116e696e725ef7865
SEO <emma05127@gmail.com>
2023-10-02 18:19:32 +0900

    [unrated] Title: 카드 뭉치, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.js
@@ -1,29 +1,9 @@
 function solution(cards1, cards2, goal) {
-    let thisGoal= goal.shift();
-    let thisCard1 = cards1.shift();
-    let thisCard2 = cards2.shift();
-    
-    
-    while(goal.length > 0){
-        while(thisCard1){
-            if(thisCard1 === thisGoal){
-                if(goal.length === 0) return'Yes'
-                thisGoal = goal.shift();
-                thisCard1 = cards1.shift();
-            } else break;
-        }
-
-        while(thisCard2){
-            if(thisCard2 === thisGoal){
-                if(goal.length === 0) return 'Yes'
-                thisGoal = goal.shift();
-                thisCard2 = cards2.shift();
-            } else break;
-        }
-        
-        if(thisCard1 !== thisGoal && thisCard2 !== thisGoal) return 'No'
+    for(const thisGoal of goal){
+        if(cards1[0] === thisGoal) cards1.shift();
+        else if(cards2[0] === thisGoal) cards2.shift();
+        else return "No"
     }
-    
     return 'Yes';
 }
 
