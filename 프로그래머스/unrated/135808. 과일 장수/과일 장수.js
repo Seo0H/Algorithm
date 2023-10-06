cb67f6bb1e726bdee76d402f61def51f6ca42dd0
SEO <emma05127@gmail.com>
2023-10-06 14:06:15 +0900

    [unrated] Title: 과일 장수, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
@@ -1,10 +1,8 @@
 function solution(k, m, score) {
-    var answer = 0;
+    let answer = 0;
     
-    // 내림차순 정렬
     score.sort((a,b)=> b-a);
     
-    // 옵션1. 각 원소가 m개가 되도록 쪼갠 후, 해당 배열을 Math.min으로 검사
     const MaxnumOffruitsInABox = score.length - (score.length % m);
     
     for(let i = m; i <= MaxnumOffruitsInABox ; i += m){
@@ -14,6 +12,3 @@ function solution(k, m, score) {
     return answer;
 }
 
-// 낮은 점수는 낮은 점수끼리
-// 높은 점수는 높은 점수끼리
-// 옵션2. 순회하며 acc에 m개 만큼 원소를 넣어 비교. 만약 acc에 m개만큼 원소가 채워졌다면 Math.min으로 값 추출
