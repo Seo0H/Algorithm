d0bd9500ca10a00af7129393071870a020c67710
SEO <emma05127@gmail.com>
2023-10-10 12:33:02 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.20 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
@@ -1,24 +1,18 @@
 function solution(new_id) {
     let answer =''
     
-    const stap2Regx = /[^\w-_\.]/g;
+    const selectUnacceptableChar = /[^\w-_\.]/g;
     const selectRepeatDot = /\.+/g;
     const selectFirstLastDot = /\.+$|^\.+/g;
+    const selectFullEmty = /^$/g;
     
-    // step 1~4
     answer = new_id.toLowerCase()
-        .replaceAll(stap2Regx,'')
+        .replaceAll(selectUnacceptableChar,'')
         .replaceAll(selectRepeatDot, '.')
+        .replaceAll(selectFirstLastDot,'')
+        .replaceAll(selectFullEmty,'a')
+        .slice(0,15)
         .replaceAll(selectFirstLastDot,'');
     
-    // step 5
-    answer = answer === '' ? 'a' : answer;
-    
-    // step 6
-    answer = answer.length >= 16 ? answer.slice(0,15).replaceAll(selectFirstLastDot,'') : answer;
-    
-    // step 7
-    answer = answer.length <= 2 ? answer + answer.at(-1).repeat(3 - answer.length) : answer;
-
-    return answer;
+    return answer.length <= 2 ? answer + answer.at(-1).repeat(3 - answer.length) : answer
 }
