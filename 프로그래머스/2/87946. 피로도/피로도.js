19aac60bcb6dfa49c1083fab8cb48fce4095e841
SEO <emma05127@gmail.com>
2023-10-13 19:30:03 +0900

    [level 2] Title: 피로도, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.js
+++ 프로그래머스/2/87946. 피로도/피로도.js
@@ -1,17 +1,17 @@
 function solution(k, dungeons) {
     var answer = 0;
     const visitied = Array.from({length:dungeons.length}, () => false);
-    bf(k, visitied, 0, 0);
+    dfs(k, visitied, 0, 0);
     
     return answer;
     
-    function bf(k, visited, count){
+    function dfs(k, visited, count){
         if(count > answer) answer = count;
         
         for(let i=0; i<dungeons.length; i++){
             if(!visited[i] && k >= dungeons[i][0]){
                 visited[i] = true;
-                bf( k - dungeons[i][1], visited, count+1);
+                dfs( k - dungeons[i][1], visited, count+1);
                 visited[i] = false;
             }
         }
