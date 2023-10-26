b4e44ca33a8d17b1be88a375cd40595071b272b8
SEO <emma05127@gmail.com>
2023-10-26 21:22:44 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 14.23 ms, Memory: 38.7 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,25 +1,41 @@
 function solution(maps) {
     var answer = 0;
-    const dir = [[0,1],[1,0],[0,-1],[-1,0]];
-    const q = [[0,0,1]];
-        
-    while(q.length){
-        const [y,x,a] = q.shift();
-        
-        if(y === maps.length-1 && x === maps[0].length-1) {
-            return a;
-        }
-        
-        for(let [dy, dx] of dir){
-            const ny = y + dy;
-            const nx = x + dx;
-            
-            if(maps[ny] && maps[ny][nx] && maps[ny][nx] === 1){
-                q.push([ny,nx, a+1]);
-                maps[ny][nx] = -1;
+
+    
+    for(let y=0; y<maps.length; y++){
+        for(let x=0; x<maps[0].length; x++){
+            if(maps[y] && maps[y][x] && maps[y][x] === 1){
+                answer = answer ? Math.min(answer, dfs(y,x)) : dfs(y,x);
             }
         }
     }
+
+    return answer;
+    
+    function dfs(y,x){
+        const dir = [[0,1],[1,0],[0,-1],[-1,0]];
+        const q = [[y,x]];
+        let a = 0;
         
-    return -1;
+        while(q.length){
+            const [y,x] = q.shift();
+            maps[y][x] = -1;
+            if(y === maps.length-1 && x === maps[0].length-1) {
+                console.log(a)
+                return ++a;
+            }
+        
+            for(let [dy, dx] of dir){
+                const ny = y + dy;
+                const nx = x + dx;
+                
+                if(maps[ny] && maps[ny][nx] && maps[ny][nx] === 1){
+                    q.push([ny,nx]);
+                    a++;
+                }
+            }
+        }
+        
+        return -1;
+   }
 }
