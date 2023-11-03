d6244aceb8d217aefc461fba6162cf5eb097a5ee
SEO <emma05127@gmail.com>
2023-11-03 12:36:46 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.js
@@ -0,0 +1,23 @@
+function solution(n, k) {
+    let answer = 0;
+    const num = n.toString(k);
+    num.split('0').forEach((cur) => {
+        if(isPrime(Number(cur))) answer++;
+    })
+    
+    return answer;
+}
+
+function isPrime(num){
+    if(num <= 1) return false;
+    for(let i=2; i <= Math.sqrt(num); i++){
+        if(num % i === 0) return false;
+    }
+    return true;
+}
+
+// 1. parseInt를 이용해 n을 k진수로 변환
+// 2. 해당 문자열 검사
+//      1. 해당 숫자 자체가 10진법으로 봤을때 소수인가? -> 1
+//      2. 0을 기준으로 나누어지는 숫자가 소수인가? splice를 0기준으로 해서 소수 검사.
+
