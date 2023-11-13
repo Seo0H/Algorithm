13a010d7b27094e71c2d67c0fc928d0a40cfbb6f
SEO <emma05127@gmail.com>
2023-11-13 10:27:54 +0900

    [level 2] Title: 기능개발, Time: 0.13 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,29 +1,24 @@
 function solution(progresses, speeds) {
-    const answer = [];
-    // 반복문으로 progresses[i] + speeds[i] 실행. 만약 100이 되면 대기.
-    // 만약 progresses 가장 앞에 있는 요소가 100이 되면 배포.
-    // 이 경우 만약 뒤에 있는 요소들이 연속으로 100이 될 경우 다같이 배포.
-    // 쭉 보내다가 만약 100이 아닌 요소를 만나면 다시 반복문 돌려서 더하기..
-    
-    while(progresses.length > 0){
-        for(let i = 0; i< progresses.length; i++)
-            if(progresses[i] < 100) progresses[i] += speeds[i];
+    var answer = [];
+    let i=0;
+    while(progresses.length){
+        if(progresses[i] < 100) progresses[i] += speeds[i];
+        i++;
         
-        if(progresses[0] >= 100) distribution();
+        if(i >= progresses.length) {
+            i=0;
+            let cnt = 0;
+            while(progresses[0] >= 100) {
+                progresses.shift();
+                speeds.shift();
+                cnt++;
+            }
+            if(cnt > 0) answer.push(cnt)
+        };
     }
-    
     return answer;
-    
-    function distribution(){
-        let count = 0;
-        while(progresses[0] >= 100){
-            progresses.shift();
-            speeds.shift();
-            count++;
-        }
-        answer.push(count);
-    }
-    
-
 }
 
+// 1. progresses 배열을 순회하며 각 요소와 해당 요소에 idx에 해당하는 speeds 요소를 더한다.
+// 2. idx가 0이고 해당 요소가 100일 경우, 앞에서부터 요소의 값이 100 이상인 값들을 제거한다.
+// 3. 제거 중 100 미만 요소가 있을 경우 멈추고, 다시 1로 돌아간다.
