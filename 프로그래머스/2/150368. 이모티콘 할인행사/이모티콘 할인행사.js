5cd3790c219297b0c0e72bf278a2e8f3201e2248
SEO <emma05127@gmail.com>
2023-11-21 14:01:53 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 0.62 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.js
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.js
@@ -28,6 +28,11 @@ function solution(users, emoticons) {
                             userSales += applyDiscount;
                         }
                     }
+                        // 유저가 원하는 할인율보다 임티 할인율 클 경우
+                        // 유저 예산에서 applyDiscount을 뺀다. 
+                        // 만약 유저 예산이 0 이하가 되면 emoPluse 를 +1 하고
+                        // 해당 유저가 구매했던 이모티콘 금액을 sales에서 제외한다. 이때 루프 문을 빠저나간다.
+                        // 아니라면 sales를 +1 한다.
                 }
                 acc.sales += userSales;
                 return acc;
