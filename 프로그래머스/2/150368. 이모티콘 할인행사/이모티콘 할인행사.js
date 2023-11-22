f96a8ebb358ca08ede0287decbd290100954d793
SEO <emma05127@gmail.com>
2023-11-22 22:28:31 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 0.34 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.js
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.js
@@ -1,43 +1,34 @@
 function solution(users, emoticons) {
-    var answer = [];
-    const discounts = [10, 20, 30, 40];
-    dfs(emoticons.length, [])
-    answer.sort((a,b) => b.emoPluse - a.emoPluse || b.sales - a.sales)
-    return [answer[0].emoPluse, answer[0].sales];
-
-    function dfs(depth,discountPer){
-
-        if(discountPer.length >= emoticons.length){
-            const applyDiscounts = emoticons.map((cur,idx) => cur * (1 - discountPer[idx] * 0.01))
-
-            answer.push(
-                users.reduce((acc, [userPercent, buget]) =>{
-                let userSales = 0;
-
-                for(let idx = 0; idx < emoticons.length ; idx++){
-                    const applyDiscount = applyDiscounts[idx];
-
-                    if(userPercent <= discountPer[idx]) {
-                        buget -= applyDiscount;
-
-                        if(buget <= 0){
-                            acc.emoPluse++;
-                            userSales = 0;
-                            break;
-                        } else {
-                            userSales += applyDiscount;
-                        }
-                    }
-                }
-                acc.sales += userSales;
-                return acc;
-            }, {emoPluse: 0, sales: 0}));
-
-            return;
-        }
-
-        for(let i=0 ; i < 4 ; i++){
-            dfs(depth-1, [...discountPer, discounts[i]]);
-        }
+  let [s, m] = [0, 0];
+  const sales = [10, 20, 30, 40];
+  const n = 4 ** emoticons.length;
+  for (let i = 0; i < n; i++) {
+    let salesIndex = i.toString(4);
+    salesIndex = "0".repeat(emoticons.length - salesIndex.length) + salesIndex;
+    const salesEmo = new Array(emoticons.length);
+    for (let j = 0; j < emoticons.length; j++) {
+      salesEmo[j] = emoticons[j] * (1 - sales[+salesIndex[j]] / 100);
     }
+    let [sSum, mSum] = [0, 0];
+    users.forEach((user) => {
+      let tempM = 0;
+      for (let k = 0; k < emoticons.length; k++) {
+        if (user[0] <= sales[+salesIndex[k]]) {
+          tempM += salesEmo[k];
+        }
+      }
+      if (tempM >= user[1]) {
+        sSum += 1;
+      } else {
+        mSum += tempM;
+      }
+    });
+    if (sSum > s) {
+      s = sSum;
+      m = mSum;
+    } else if (sSum === s && mSum > m) {
+      m = mSum;
+    }
+  }
+  return [s, m];
 }
