4a7a64f9b6bf9f954abfd1273ae771804ce7d5a5
SEO <emma05127@gmail.com>
2023-11-22 22:27:29 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 0.35 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.js
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.js
@@ -1,55 +1,43 @@
 function solution(users, emoticons) {
     var answer = [];
     const discounts = [10, 20, 30, 40];
-    
-    emoticons = emoticons.map((price) => discounts.reduce((acc, percent)=> {
-        acc[1].set(percent, price * (1 - percent * 0.01));
-        return acc;
-    },[price, new Map]));
-    
     dfs(emoticons.length, [])
     answer.sort((a,b) => b.emoPluse - a.emoPluse || b.sales - a.sales)
     return [answer[0].emoPluse, answer[0].sales];
-    
-    function dfs(depth,discountPerArray){
 
-        if(discountPerArray.length >= emoticons.length){
-            
+    function dfs(depth,discountPer){
+
+        if(discountPer.length >= emoticons.length){
+            const applyDiscounts = emoticons.map((cur,idx) => cur * (1 - discountPer[idx] * 0.01))
+
             answer.push(
                 users.reduce((acc, [userPercent, buget]) =>{
                 let userSales = 0;
-                
+
                 for(let idx = 0; idx < emoticons.length ; idx++){
-                    const discountPercent = discountPerArray[idx];
-                    const appliedDiscount = emoticons[idx][1].get(discountPercent);
-                          
-                    if(userPercent <= discountPercent) {
-                        buget -= appliedDiscount;
-                        
+                    const applyDiscount = applyDiscounts[idx];
+
+                    if(userPercent <= discountPer[idx]) {
+                        buget -= applyDiscount;
+
                         if(buget <= 0){
                             acc.emoPluse++;
                             userSales = 0;
                             break;
                         } else {
-                            userSales += appliedDiscount;
+                            userSales += applyDiscount;
                         }
                     }
-                        // 유저가 원하는 할인율보다 임티 할인율 클 경우
-                        // 유저 예산에서 applyDiscount을 뺀다. 
-                        // 만약 유저 예산이 0 이하가 되면 emoPluse 를 +1 하고
-                        // 해당 유저가 구매했던 이모티콘 금액을 sales에서 제외한다. 이때 루프 문을 빠저나간다.
-                        // 아니라면 sales를 +1 한다.
                 }
                 acc.sales += userSales;
                 return acc;
             }, {emoPluse: 0, sales: 0}));
-            
+
             return;
         }
-        
+
         for(let i=0 ; i < 4 ; i++){
-            dfs(depth-1, [...discountPerArray, discounts[i]]);
+            dfs(depth-1, [...discountPer, discounts[i]]);
         }
     }
 }
-
