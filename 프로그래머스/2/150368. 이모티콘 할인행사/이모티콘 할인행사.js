f6a971f27289590b6972cb289ef66ed08e7ee3a2
SEO <emma05127@gmail.com>
2023-11-22 21:35:16 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 0.42 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.js
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.js
@@ -1,49 +1,52 @@
 function solution(users, emoticons) {
-    var answer = [];
+    let answer = [];
     const discounts = [10, 20, 30, 40];
-    dfs(emoticons.length, [])
-    answer.sort((a,b) => b.emoPluse - a.emoPluse || b.sales - a.sales)
-    return [answer[0].emoPluse, answer[0].sales];
-    
-    function dfs(depth,discountPer){
+    const maxDiscountPermutations = Math.pow(4, emoticons.length);
+    const memo = new Array(maxDiscountPermutations).fill(null).map(() => new Map());
 
-        if(discountPer.length >= emoticons.length){
-            const applyDiscounts = emoticons.map((cur,idx) => cur * (1 - discountPer[idx] * 0.01))
-            
-            answer.push(
-                users.reduce((acc, [userPercent, buget]) =>{
-                let userSales = 0;
-                
-                for(let idx = 0; idx < emoticons.length ; idx++){
-                    const applyDiscount = applyDiscounts[idx];
-                    
-                    if(userPercent <= discountPer[idx]) {
-                        buget -= applyDiscount;
-                        
-                        if(buget <= 0){
-                            acc.emoPluse++;
-                            userSales = 0;
-                            break;
-                        } else {
-                            userSales += applyDiscount;
+    dfs(0, 0, []);
+
+    answer.sort((a, b) => b.emoPlus - a.emoPlus || b.sales - a.sales);
+    return [answer[0].emoPlus, answer[0].sales];
+
+    function dfs(idx, discountPer, curDiscounts) {
+        if (idx >= emoticons.length) {
+            const discountKey = curDiscounts.join(',');
+            if (memo[idx].has(discountKey)) {
+                return memo[idx].get(discountKey);
+            }
+
+            const applyDiscounts = emoticons.map((cur, idx) => cur * (1 - curDiscounts[idx] * 0.01));
+            const result = users.reduce(
+                (acc, [userPercent, budget]) => {
+                    let userSales = 0;
+
+                    for (let idx = 0; idx < emoticons.length; idx++) {
+                        const applyDiscount = applyDiscounts[idx];
+
+                        if (userPercent <= curDiscounts[idx]) {
+                            budget -= applyDiscount;
+
+                            if (budget <= 0) {
+                                acc.emoPlus++;
+                                userSales = 0;
+                                break;
+                            } else userSales += applyDiscount;
                         }
                     }
-                        // 유저가 원하는 할인율보다 임티 할인율 클 경우
-                        // 유저 예산에서 applyDiscount을 뺀다. 
-                        // 만약 유저 예산이 0 이하가 되면 emoPluse 를 +1 하고
-                        // 해당 유저가 구매했던 이모티콘 금액을 sales에서 제외한다. 이때 루프 문을 빠저나간다.
-                        // 아니라면 sales를 +1 한다.
-                }
-                acc.sales += userSales;
-                return acc;
-            }, {emoPluse: 0, sales: 0}));
-            
-            return;
+                    acc.sales += userSales;
+                    return acc;
+                },
+                { emoPlus: 0, sales: 0 },
+            );
+
+            memo[idx].set(discountKey, result);
+            answer.push(result);
+            return result;
         }
-        
-        for(let i=0 ; i < 4 ; i++){
-            dfs(depth-1, [...discountPer, discounts[i]]);
+
+        for (let i = 0; i < 4; i++) {
+            dfs(idx + 1, i, [...curDiscounts, discounts[i]]);
         }
     }
 }
-
