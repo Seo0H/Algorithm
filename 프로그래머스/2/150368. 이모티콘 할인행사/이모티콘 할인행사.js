73da3c1329b4853fe22eedfd305b0840bd6b9b99
SEO <emma05127@gmail.com>
2023-11-23 21:15:26 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 0.38 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.js
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.js
@@ -1,34 +1,52 @@
+
 function solution(users, emoticons) {
-  let [s, m] = [0, 0];
-  const sales = [10, 20, 30, 40];
-  const n = 4 ** emoticons.length;
-  for (let i = 0; i < n; i++) {
-    let salesIndex = i.toString(4);
-    salesIndex = "0".repeat(emoticons.length - salesIndex.length) + salesIndex;
-    const salesEmo = new Array(emoticons.length);
-    for (let j = 0; j < emoticons.length; j++) {
-      salesEmo[j] = emoticons[j] * (1 - sales[+salesIndex[j]] / 100);
-    }
-    let [sSum, mSum] = [0, 0];
-    users.forEach((user) => {
-      let tempM = 0;
-      for (let k = 0; k < emoticons.length; k++) {
-        if (user[0] <= sales[+salesIndex[k]]) {
-          tempM += salesEmo[k];
-        }
-      }
-      if (tempM >= user[1]) {
-        sSum += 1;
-      } else {
-        mSum += tempM;
-      }
-    });
-    if (sSum > s) {
-      s = sSum;
-      m = mSum;
-    } else if (sSum === s && mSum > m) {
-      m = mSum;
-    }
-  }
-  return [s, m];
+	let answer = {};
+	const discounts = [10, 20, 30, 40];
+	dfs(emoticons.length, []);
+	return [answer.emoPlus, answer.sales];
+
+	function dfs(depth, discountPer) {
+		if (discountPer.length >= emoticons.length) {
+			const applyDiscounts = emoticons.map((cur, idx) => cur * (1 - discountPer[idx] * 0.01));
+
+			const calculateSales = users.reduce(
+				(acc, [userPercent, budget]) => {
+					let userSales = 0;
+
+					for (let idx = 0; idx < emoticons.length; idx++) {
+						const applyDiscount = applyDiscounts[idx];
+
+						if (userPercent <= discountPer[idx]) {
+							budget -= applyDiscount;
+
+							if (budget <= 0) {
+								acc.emoPlus++;
+								userSales = 0;
+								break;
+							} else {
+								userSales += applyDiscount;
+							}
+						}
+					}
+					acc.sales += userSales;
+					return acc;
+				},
+				{ emoPlus: 0, sales: 0 },
+			);
+
+			if (answer.emoPlus === undefined) {
+				answer = calculateSales;
+			} else if (answer.emoPlus < calculateSales.emoPlus) {
+				answer = calculateSales;
+			} else if (answer.emoPlus === calculateSales.emoPlus) {
+				answer = answer.sales < calculateSales.sales ? calculateSales : answer;
+			}
+
+			return;
+		}
+
+		for (let i = 0; i < 4; i++) {
+			dfs(depth - 1, [...discountPer, discounts[i]]);
+		}
+	}
 }
