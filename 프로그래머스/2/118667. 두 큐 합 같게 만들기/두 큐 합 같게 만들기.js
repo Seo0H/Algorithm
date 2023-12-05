c0e95e911a2e5ff1731c059e8fc60cf88baf560c
Seoyoung Hwang / Seo <emma05127@gmail.com>
2023-12-05 10:15:00 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 20.13 ms, Memory: 47.8 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
@@ -1,29 +1,37 @@
 function solution(queue1, queue2) {
-    let q1sum = queue1.reduce((acc,cur) => acc+cur, 0);
-    let q2sum = queue2.reduce((acc,cur) => acc+cur, 0);
-
-    if((q1sum+q2sum)%2 === 1) return -1;
-
-    const aimNum = (q1sum + q2sum) / 2;
-    const maxLoop = queue1.length*3+1;
-    let [q1,q2] = [0,0];
-
-
+    let sumQ1 = queue1.reduce((acc,cur) => acc + cur,0)
+    let sumQ2 = queue2.reduce((acc,cur) => acc + cur,0);
+    
+    if((sumQ1 + sumQ2)%2 === 1) return -1
+    
+    const len = queue1.length;
+    const maxLoop = (len+len)*2;
+    let [q1idx, q2idx]= [0,0];
+    
     for(let i=0; i<maxLoop; i++){
-        if(q1sum === q2sum){
-            return i;
-        } else if(q1sum > q2sum){
-            let x = queue1[q1++];
-            q1sum -= x;
-            q2sum += x;
-            queue2.push(x);
-        }else {
-            let x = queue2[q2++]
-            q2sum -= x;
-            q1sum += x;
-            queue1.push(x);
+        if(sumQ1 === sumQ2) return i;
+        
+        if(sumQ1 > sumQ2){
+            const popQ1Val = queue1[q1idx++];
+            queue2.push(popQ1Val);
+            sumQ2 += popQ1Val;
+            sumQ1 -= popQ1Val;
+            continue;
+        } 
+        
+        if(sumQ1 < sumQ2){
+            const popQ2Val = queue2[q2idx++];
+            queue1.push(popQ2Val);
+            sumQ1 += popQ2Val;
+            sumQ2 -= popQ2Val;
+            continue;
         }
     }
-
+    
     return -1;
 }
+
+// 큐1 , 큐2 의 값을 더한 각각의 변수 -> sumQ1, sumQ2
+// sum.. 변수가 더 큰 쪽에서 더 작은쪽으로 무조건 보내야 함
+// 아무리 작업해도 안되는 경우는 어떻게 측정할건지 - 큐의 길이 ^ 2
+// (signal: aborted (core dumped)) -> 산술 오버플로우 발생 -> Big int 안먹힘
