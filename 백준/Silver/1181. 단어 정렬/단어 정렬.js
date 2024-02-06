38e8f7fea80a13c7c99b1e6748ba08e1db2009a4
Seoyoung Hwang / Seo <emma05127@gmail.com>
2024-02-06 14:39:32 +0900

    [Silver V] Title: 단어 정렬, Time: 256 ms, Memory: 19960 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.js
@@ -0,0 +1,12 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+const N = Number(input.shift());
+
+input.sort((a, b) => {
+  return a.length - b.length || a.localeCompare(b);
+});
+
+const set = new Set(input);
+
+console.log(Array.from(set).join("\n"));
