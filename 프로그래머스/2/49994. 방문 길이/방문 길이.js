6a1d70042d560cbd8d3d98a350cbd978f5201083
Seoyoung Hwang / Seo <emma05127@gmail.com>
2024-03-14 14:04:04 +0900

    [level 2] Title: 방문 길이, Time: 1.22 ms, Memory: 34 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.js
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.js
@@ -3,27 +3,12 @@ function solution(dirs) {
        x: 0,
        y:0
    };
-    
-    const dirTemplet = {
-        U: [0,0.5],
-        D: [0,-0.5],
-        R: [0.5,0],
-        L: [-0.5,0]
-    }
-    
-    const isOkayToLog = {
-        U: (x,y) => !(y === 5),
-        D: (x,y) => !(y === -5),
-        R: (x,y) => !(x === 5),
-        L: (x,y) => !(x === -5)
-    }
-    
-    
+
     return dirs.split("").reduce((acc,dir) => {
         const { x , y } = coordinates;
-        const [weightsX, weightsY] = dirTemplet[dir]
+        const {weights: [weightsX, weightsY], isOkayToLog} = getDirUtils(dir)
         
-        if(isOkayToLog[dir](x,y)){
+        if(isOkayToLog(x,y)){
             acc.add(`${x + weightsX}_${y + weightsY}`);
             coordinates.x = x + weightsX * 2;
             coordinates.y = y + weightsY * 2;
@@ -33,3 +18,20 @@ function solution(dirs) {
     }, new Set()).size
 }
 
+function getDirUtils (dir) {
+    const dirTemplet = {
+        U: [0,0.5],
+        D: [0,-0.5],
+        R: [0.5,0],
+        L: [-0.5,0]
+    }
+
+    const isOkayToLog = {
+        U: (x,y) => !(y === 5),
+        D: (x,y) => !(y === -5),
+        R: (x,y) => !(x === 5),
+        L: (x,y) => !(x === -5)
+    }
+
+    return {weights : dirTemplet[dir], isOkayToLog: isOkayToLog[dir]}
+}
