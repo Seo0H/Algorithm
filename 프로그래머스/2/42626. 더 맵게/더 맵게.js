2077291ac85f164f67bb8924091417dec43d5964
Seoyoung Hwang / Seo <emma05127@gmail.com>
2024-03-21 20:06:42 +0900

    [level 2] Title: 더 맵게, Time: 1388.36 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.js
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.js
@@ -1,83 +1,104 @@
-function solution(scoville, K) {
-  let answer = 0;
-  const minHeap = new MinHeap();
-
-  // minHeap.set(scoville);
-    scoville.forEach(el => minHeap.push(el));
-
-  if (minHeap.peek() >= K) return answer;
-
-  while (minHeap.peek() < K && minHeap.size() > 1) {
-    answer++;
-    const mixed = minHeap.pop() + minHeap.pop() * 2;
-    minHeap.push(mixed);
+class NumberMinHeap {
+  /**
+   * @param {number[]} val
+   */
+  constructor(val) {
+    this.heap = [];
+    val.forEach((el) => this.insert(el));
+    // this._bubbleDown();
   }
 
-  return minHeap.peek() >= K ? answer : -1;
+  size = () => this.heap.length;
+
+  pop() {
+    if (!this.size()) return null;
+    if (this.size() === 1) return this.heap.pop();
+
+    const result = this.heap[0];
+    this.heap[0] = this.heap.pop();
+    this._bubbleDown();
+
+    return result;
+  }
+
+  peek() {
+    return this.heap[0];
+  }
+
+  /**
+   * @param {number} newVal
+   */
+  insert(newVal) {
+    this.heap.push(newVal);
+    this._bubbleUp();
+  }
+
+  _getParentIdx = (childIdx) => Math.floor((childIdx - 1) / 2);
+  _getRightChildIdx(parentIdx) {
+    const rightChildIdx = parentIdx * 2 + 2;
+    if (rightChildIdx > this.size() - 1) return undefined;
+    return rightChildIdx;
+  }
+  _getLeftChildIdx(parentIdx) {
+    const leftChildIdx = parentIdx * 2 + 1;
+    if (leftChildIdx > this.size() - 1) return undefined;
+    return leftChildIdx;
+  }
+
+  _bubbleDown() {
+    let parentIdx = 0;
+    let leftChildIdx = this._getLeftChildIdx(parentIdx);
+    let rightChildIdx = this._getRightChildIdx(parentIdx);
+
+    // 왼쪽과 오른쪽 자식 노드 둘 중 하나라도 부모 노드보다 작을 경우 while 문을 반복한다.
+    const isLeftChild = leftChildIdx <= this.size() - 1;
+    const isRightChild = rightChildIdx <= this.size() - 1;
+
+    while (
+      (isLeftChild && this.heap[leftChildIdx] < this.heap[parentIdx]) ||
+      (isRightChild && this.heap[rightChildIdx] < this.heap[parentIdx])
+    ) {
+      // 오른쪽 자식노드가 왼쪽 자식노드보다 작은 경우
+      if (isRightChild && this.heap[leftChildIdx] > this.heap[rightChildIdx]) {
+        this._swap(parentIdx, rightChildIdx);
+        parentIdx = rightChildIdx;
+      } else {
+        this._swap(parentIdx, leftChildIdx);
+        parentIdx = leftChildIdx;
+      }
+
+      leftChildIdx = this._getLeftChildIdx(parentIdx);
+      rightChildIdx = this._getRightChildIdx(parentIdx);
+    }
+  }
+
+  //아래에서부터 위로 정렬
+  _bubbleUp() {
+    let childIdx = this.size() - 1;
+    let parentIdx = this._getParentIdx(childIdx);
+
+    while (this.heap[childIdx] < this.heap[parentIdx]) {
+      this._swap(childIdx, parentIdx);
+      childIdx = parentIdx;
+      parentIdx = this._getParentIdx(parentIdx);
+    }
+  }
+
+  _swap(idx1, idx2) {
+    [this.heap[idx1], this.heap[idx2]] = [this.heap[idx2], this.heap[idx1]];
+  }
 }
 
-function MinHeap() {
-  let heap = [];
-  const swap = (idx1, idx2) =>
-    ([heap[idx1], heap[idx2]] = [heap[idx2], heap[idx1]]);
-  const getParentIdx = (childIdx) => Math.floor((childIdx - 1) / 2);
-  const getLeftChildIdx = (parentIdx) => parentIdx * 2 + 1;
-  const getRightChildIdx = (parentIdx) => parentIdx * 2 + 2;
+function solution(scoville, K) {
+  const scovilleMinHeap = new NumberMinHeap(scoville);
+  let answer = 0;
+    
+  if (scovilleMinHeap.peek() >= K) return answer;
 
-  return {
-    size() {
-      return heap.length;
-    },
-    peek() {
-      return heap[0];
-    },
-    pop() {
-      if (!this.size()) return null;
-      if (this.size() === 1) return heap.pop();
-      const value = this.peek();
-      heap[0] = heap.pop();
-      this.bubbleDown();
-      return value;
-    },
-    push(value) {
-      heap.push(value);
-      this.bubbleUp();
-    },
-    bubbleUp() {
-      let childIdx = this.size() - 1;
-      let parentIdx = getParentIdx(childIdx);
+  while (scovilleMinHeap.peek() < K && scovilleMinHeap.size() > 1) {
+    answer += 1;
+    scovilleMinHeap.insert(scovilleMinHeap.pop() + scovilleMinHeap.pop() * 2);
+  }
 
-      while (heap[childIdx] < heap[parentIdx]) {
-        swap(childIdx, parentIdx);
-        childIdx = parentIdx;
-        parentIdx = getParentIdx(parentIdx);
-      }
-    },
-    bubbleDown() {
-      let parent = 0;
-      let leftChildIdx = getLeftChildIdx(parent);
-      let rightChildIdx = getRightChildIdx(parent);
-
-      // 왼쪽과 오른쪽 자식 노드 둘 중 하나라도 부모 노드보다 작을 경우 while 문을 반복한다.
-      const isLeftChild = leftChildIdx <= this.size() - 1;
-      const isRightChild = rightChildIdx <= this.size() - 1;
-
-      while (
-        (isLeftChild && heap[leftChildIdx] < heap[parent]) ||
-        (isRightChild && heap[rightChildIdx] < heap[parent])
-      ) {
-        // 오른쪽 자식노드가 왼쪽 자식노드보다 작은 경우
-        if (isRightChild && heap[leftChildIdx] > heap[rightChildIdx]) {
-          swap(parent, rightChildIdx);
-          parent = rightChildIdx;
-        } else {
-          swap(parent, leftChildIdx);
-          parent = leftChildIdx;
-        }
-
-        leftChildIdx = getLeftChildIdx(parent);
-        rightChildIdx = getRightChildIdx(parent);
-      }
-    },
-  };
+  return scovilleMinHeap.peek() >= K ? answer : -1;
 }
