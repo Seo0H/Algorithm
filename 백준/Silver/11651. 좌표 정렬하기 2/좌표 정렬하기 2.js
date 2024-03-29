8832b0162ebea26ec5dc6dc426c2f68bad691e95
Seoyoung Hwang / Seo <emma05127@gmail.com>
2024-03-29 09:40:19 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 668 ms, Memory: 67980 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+
+const filePath =
+  process.platform === "linux" ? "./dev/stdin" : `${__dirname}/input.txt`;
+
+const input = fs
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n")
+  .map((el) => el.split(" ").map(Number));
+
+console.log(solution(input.shift().pop(), input));
+
+/**
+ * @param {number} N
+ * @param {[number, number]} coordinates
+ */
+function solution(N, coordinates) {
+  return coordinates
+    .sort(([ax, ay], [bx, by]) => (ay - by ? ay - by : ax - bx))
+    .map((el) => el.join(" "))
+    .join("\n");
+}
