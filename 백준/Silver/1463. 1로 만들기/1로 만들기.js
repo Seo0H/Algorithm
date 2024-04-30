081a66364faa11a69f372f54462997c15d69b325
Seoyoung Hwang / Seo <emma05127@gmail.com>
2024-04-30 09:34:12 +0900

    [Silver III] Title: 1로 만들기, Time: 276 ms, Memory: 29432 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+
+const filePath =
+  process.platform === "linux" ? "./dev/stdin" : `${__dirname}/input.txt`;
+
+const input = fs.readFileSync(filePath).toString().trim();
+
+console.log(solution(Number(input)));
+
+/**
+ * @param {number} N
+ */
+function solution(N) {
+  const dp = Array.from({ length: N }, () => 0);
+
+  for (let i = 2; i <= N; i++) {
+    dp[i] = dp[i - 1] + 1;
+
+    if (i % 2 === 0) {
+      dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+    }
+
+    if (i % 3 === 0) {
+      dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+    }
+  }
+
+  return dp[N] ? dp[N] : 0;
+}
