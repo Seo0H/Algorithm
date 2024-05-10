37d84f36d9ab84b0aa6432bf62e2596815b468d8
Seoyoung Hwang / Seo <emma05127@gmail.com>
2024-05-10 13:35:49 +0900

    [Silver III] Title: 2×n 타일링, Time: 228 ms, Memory: 9644 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+
+const filePath =
+  process.platform === "linux" ? "./dev/stdin" : `${__dirname}/input.txt`;
+
+const input = fs.readFileSync(filePath).toString().trim();
+
+console.log(solution(Number(input)));
+
+/**
+ * @param {number} N
+ */
+function solution(N) {
+  const dp = [];
+  dp[1] = 1;
+  dp[2] = 2;
+
+  for (let i = 3; i <= N; i++) {
+    dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+  }
+  return dp[N];
+}
