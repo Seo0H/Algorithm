ee92c8fc1dd0e863991784fb4bc5596780486f9f
Seoyoung Hwang / Seo <emma05127@gmail.com>
2024-05-17 17:15:41 +0900

    [level 2] Title: 모음 사전, Time: 3.11 ms, Memory: 37.3 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.js
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.js
@@ -1,5 +1,19 @@
-function solution(words) {
-    return words
-        .split('')
-        .reduce((r, c, i) => r + [781, 156, 31, 6, 1][i] * ['A', 'E', 'I', 'O', 'U'].indexOf(c) + 1, 0);
+const word = ['A', 'E', 'I', 'O', 'U'];
+let count = 0
+
+function solution(word) {
+    const result = dfs('')
+    return result.get(word)
+}
+
+
+function dfs(cur, logs=new Map()){
+    if(cur.length > 5) return
+    logs.set(cur, count);
+    count+=1;
+    for(let i=0; i<5; i++){
+    dfs(cur + word[i], logs)
+    }
+    
+    return logs
 }
