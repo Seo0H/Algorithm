a7476f920dc7ad3d51b0304df34a5e2c4a7b8738
Seoyoung Hwang / Seo <emma05127@gmail.com>
2024-05-17 14:32:04 +0900

    [Silver I] Title: 숨바꼭질, Time: 3328 ms, Memory: 25968 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
@@ -0,0 +1,39 @@
+const fs = require("fs");
+
+const filePath =
+  process.platform === "linux" ? "./dev/stdin" : `${__dirname}/input.txt`;
+
+const input = fs
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split(" ")
+  .map(Number);
+
+console.log(solution(input));
+
+/**
+ * @param {[number, number]} input
+ */
+function solution([number, target]) {
+  const q = [[number, 0]]; // [curNum, count]
+  const visited = new Map();
+  visited.set(number, true);
+
+  while (q.length) {
+    const [cur, curCount] = q.shift();
+
+    if (cur === target) {
+      return curCount;
+    }
+
+    for (let next of [cur - 1, cur + 1, cur * 2]) {
+      if (next >= 0 && next <= 100000 && !visited.has(next)) {
+        visited.set(next, true);
+        q.push([next, curCount + 1]);
+      }
+    }
+  }
+
+  return 0;
+}
