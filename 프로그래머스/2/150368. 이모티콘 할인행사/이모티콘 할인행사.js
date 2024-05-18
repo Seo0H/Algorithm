422e73e38b63f206e8e47556147fd69f382ac532
Seoyoung Hwang / Seo <emma05127@gmail.com>
2024-05-18 20:59:39 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 0.74 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.js
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.js
@@ -1,73 +1,60 @@
-/*
- * @url https://school.programmers.co.kr/learn/courses/30/lessons/150368
- * @date 23-11-22
- */
-
 function solution(users, emoticons) {
-	var answer = [];
-	const discounts = [10, 20, 30, 40];
-	dfs();
-	// sort 와 if문의 효율성 차이를 비교하기 위해 주석 처리
-	// answer.sort((a, b) => b.emoPlus - a.emoPlus || b.sales - a.sales);
-	return [answer.emoPlus, answer.sales];
-
-	function dfs(discountPermutation = []) {
-		if (discountPermutation.length === emoticons.length) {
-			const emojiAppliedDiscounts = emoticons.map(
-				(cur, idx) => cur * (1 - discountPermutation[idx] * 0.01),
-			);
-
-			const calculateSales = users.reduce(
-				(acc, [userPercent, budget]) => {
-					let userSales = 0;
-
-					for (let idx = 0; idx < emoticons.length; idx++) {
-						const emojiAppliedDiscount = emojiAppliedDiscounts[idx];
-
-						// 유저가 원하는 할인율보다 조합된 할인이 클 경우
-						if (userPercent <= discountPermutation[idx]) {
-							budget -= emojiAppliedDiscount;
-
-							if (budget <= 0) {
-								acc.emoPlus++;
-								userSales = 0;
-								break;
-							}
-                            
-                            if(budget > 0) userSales += emojiAppliedDiscount;
-						}
-					}
-					acc.sales += userSales;
-					return acc;
-				},
-				{ emoPlus: 0, sales: 0 },
-			);
+    var answer = [];
+    // 조합 배열 만들기
+    const discountComb = getCombFromArray(emoticons, emoticons.length) // number[][]
+    const resultHash = discountComb.reduce((logs, discounts) => {
+        const discountedEmojis = emoticons.map(applyDiscount(discounts));
+    
+        const result = users.reduce((logs, [wantDiscount, buget]) => {
+            const userLog = discountedEmojis.reduce((logs, emojiPrice ,idx) => {
+                const discountPercent = discounts[idx];
+                
+                if(logs.emoticonPlus) return logs;
+                if(wantDiscount <= discountPercent){
+                    logs.amountPurchased += emojiPrice;
+                    buget -= emojiPrice
+                }
+                
+                if(buget <= 0){
+                    logs.emoticonPlus = true;
+                }
+                return logs
+            }, {amountPurchased: 0, emoticonPlus: false}) 
             
-            // answer.push(calculateSales)
-
-			// 초기 값 판별
-			if (answer.emoPlus === undefined) {
-				answer = calculateSales;
-                return;
-			}
-
-			if (answer.emoPlus < calculateSales.emoPlus) {
-				answer = calculateSales;
-                return;
-			}
-
-			// 우선순위가 높은 emoPlus의 구매 횟수가 같은 경우
-			if (answer.emoPlus === calculateSales.emoPlus) {
-				// 매출액 기준으로 높은 요소를 answer에 할당한다.
-				answer = answer.sales < calculateSales.sales ? calculateSales : answer;
-                return;
-			}
+            if(userLog.emoticonPlus) {
+                logs.emoticonCount += 1;
+            } else {
+                logs.emoticonSales += userLog.amountPurchased
+            }
             
-            return;
-		}
+            return logs
+            
+        }, {emoticonCount: 0, emoticonSales: 0}) 
 
-		for (let i = 0; i < 4; i++) {
-			dfs([...discountPermutation, discounts[i]]);
-		}
-	}
+        logs.push(result)
+        return logs
+    }, [])
+    
+    resultHash.sort((a,b) => b.emoticonCount - a.emoticonCount || b.emoticonSales - a.emoticonSales)
+
+    return [resultHash[0].emoticonCount, resultHash[0].emoticonSales];
+}
+
+function applyDiscount(discounts){
+    return (emoticon, idx) => emoticon - Math.floor(discounts[idx]*0.01*emoticon);
+}
+
+const discounts = [10,20,30,40]; // 10 20 30 40
+// 주어진 배열 인자로 만들 수 있는 조합을 출력하는 함수
+function getCombFromArray(arr, count, eachReault = [], finalResult=[]){
+    if(count === 0){
+        finalResult.push(eachReault)
+        return;
+    }
+
+    for(let i=0; i<4; i++){
+        getCombFromArray(arr, count-1, [...eachReault, discounts[i]], finalResult)
+    }
+    
+    return finalResult
 }
