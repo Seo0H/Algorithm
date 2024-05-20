2726f1fe3d7e51978e1d9a18d27807f565c3f043
Seoyoung Hwang / Seo <emma05127@gmail.com>
2024-05-20 23:16:47 +0900

    [level 1] Title: 옹알이 (2), Time: 0.59 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.js
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.js
@@ -1,23 +1,12 @@
 function solution(babbling) {
-    return babbling.reduce((counts, cur) => {
-        let beforeStr = ''
-        while(cur.length > 0){
-            const curStr = getCurStr(cur)
-            if(!curStr || beforeStr === curStr) break
-            beforeStr = curStr
-            cur = cur.slice(curStr.length)
+    const babblingSet = ['aya', 'ye', 'woo', 'ma'];
+    return babbling.reduce((counts, curBabble) => {
+        for(let i=0; i< babblingSet.length; i++){
+            const isRepeated = new RegExp(babblingSet[i].repeat(2)).test(curBabble)
+            if(isRepeated) return counts
+            curBabble = curBabble.split(babblingSet[i]).join(' ').trim()
         }
-        
-        return cur === '' ? counts+1 : counts
+        if(curBabble) return counts
+        return counts + 1
     }, 0)
-}
-              
-
-function getCurStr(str) {
-    const isAya = str[0] === 'a' && str[1] === 'y' && str[2] === 'a'
-    const isYe = str[0] === 'y' && str[1] === 'e'
-    const isWoo =  str[0] === 'w' && str[1] === 'o' && str[2] === 'o'
-    const isMa = str[0] === 'm' && str[1] === 'a'
-    
-    return  isAya ? 'aya' : isYe ? 'ye' : isWoo ? 'woo' : isMa ? 'ma' : false
 }
