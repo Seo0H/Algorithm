97c6edade830eafc4d017a46724c28e7f9fdea87
Seoyoung Hwang / Seo <emma05127@gmail.com>
2024-05-20 23:21:55 +0900

    [level 1] Title: 옹알이 (2), Time: 0.22 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.js
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.js
@@ -1,12 +1,8 @@
 function solution(babbling) {
-    const babblingSet = ['aya', 'ye', 'woo', 'ma'];
-    return babbling.reduce((counts, curBabble) => {
-        for(let i=0; i< babblingSet.length; i++){
-            const isRepeated = new RegExp(babblingSet[i].repeat(2)).test(curBabble)
-            if(isRepeated) return counts
-            curBabble = curBabble.split(babblingSet[i]).join(' ').trim()
-        }
-        if(curBabble) return counts
-        return counts + 1
-    }, 0)
+  const isRepeat = /(aya|ye|woo|ma)\1+/;
+  const isBabbling = /^(aya|ye|woo|ma)+$/;
+
+  return babbling.reduce((ans, word) => (
+    !isRepeat.test(word) && isBabbling.test(word) ? ++ans : ans
+  ), 0);
 }
