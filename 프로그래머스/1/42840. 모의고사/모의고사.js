f1b6bf4d9d8ec942dd614a8e57ec5d4fc56f3973
Seoyoung Hwang / Seo <emma05127@gmail.com>
2024-09-07 16:14:58 +0900

    [level 1] Title: 모의고사, Time: 6.84 ms, Memory: 37.5 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,25 +1,18 @@
+
 const patterns = {
-    1 : [1, 2, 3, 4, 5],
-    2 : [2, 1, 2, 3, 2, 4, 2, 5],
-    3 : [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-}
+  1: [1, 2, 3, 4, 5],
+  2: [2, 1, 2, 3, 2, 4, 2, 5],
+  3: [3, 3, 1, 1, 2, 2, 4, 4, 5, 5],
+};
 
 function solution(answers) {
-  const noIdx = { 1: 0, 2: 0, 3: 0 };
-
-  const resultMap = answers.reduce(getUserAnswerMap(noIdx), {
-    1: 0,
-    2: 0,
-    3: 0,
-  });
-
-  const sortedResults = getScoreDESCResult(resultMap);
-  const [no] = sortedResults.at(0);
+  const sortedDESCResults = getScoreDESCResult(getResultMap(answers));
+  const [no] = sortedDESCResults.at(0);
   const results = [no];
 
-  for (let i = 1; i < sortedResults.length; i++) {
-    const [_, biggerScore] = sortedResults[i - 1];
-    const [curNo, curScore] = sortedResults[i];
+  for (let i = 1; i < sortedDESCResults.length; i++) {
+    const [_, biggerScore] = sortedDESCResults[i - 1];
+    const [curNo, curScore] = sortedDESCResults[i];
 
     if (biggerScore === curScore) {
       results.push(curNo);
@@ -29,28 +22,46 @@ function solution(answers) {
   return results.map(Number);
 }
 
-// 스코어 내림차순 정렬
+/**
+ * @param {Object.<string, number>} resultMap key는 수포자 번호, value는 수포자 score
+ * @description score 기준 내림차순 정렬된 배열
+ */
 const getScoreDESCResult = (resultMap) =>
   Object.entries(resultMap).sort(([, aScore], [, bScore]) => bScore - aScore);
 
-const getUserAnswerMap = (noIdx) => (memo, testAnswer) => {
-  for (let no in noIdx) {
-    // 현재 수포자의 패턴
-    const abandonerPattern = patterns[no];
+/**
+ * @param {number[]} answers
+ * @returns {Object.<string, number>} resultMap. key는 수포자 번호, value는 수포자 score
+ */
+const getResultMap = (answers) => {
+  const noIdxMap = { 1: 0, 2: 0, 3: 0 };
 
-    // idx 검증
-    if (noIdx[no] >= abandonerPattern.length) {
-      noIdx[no] = 0;
+  return answers.reduce(
+    (memo, testAnswer) => {
+      for (let no in noIdxMap) {
+        // 현재 수포자의 패턴
+        const abandonerPattern = patterns[no];
+
+        // idx 검증
+        if (noIdxMap[no] >= abandonerPattern.length) {
+          noIdxMap[no] = 0;
+        }
+
+        const curIdx = noIdxMap[no];
+        noIdxMap[no] += 1;
+
+        // 정답 평가
+        if (abandonerPattern[curIdx] === testAnswer) {
+          memo[no] += 1;
+        }
+      }
+
+      return memo;
+    },
+    {
+      1: 0,
+      2: 0,
+      3: 0,
     }
-
-    const curIdx = noIdx[no];
-    noIdx[no] += 1;
-
-    // 정답 평가
-    if (abandonerPattern[curIdx] === testAnswer) {
-      memo[no] += 1;
-    }
-  }
-
-  return memo;
+  );
 };
