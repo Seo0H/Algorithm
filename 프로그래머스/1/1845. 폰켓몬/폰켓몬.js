b684c1aaa8214cb42cfab0a450f632e017b56232
Seoyoung Hwang / Seo <emma05127@gmail.com>
2025-03-10 21:33:13 +0900

    [level 1] Title: 폰켓몬, Time: 0.64 ms, Memory: 34.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,15 +1,6 @@
 function solution(nums) {
     const n = nums.length / 2;
-    const ponketmonMap = nums.reduce((acc,cur) => {
-        if(acc[cur]){
-            acc[cur] += 1;
-        } else{
-            acc[cur] = 1;
-        }
-        return acc
-    }, {})
+    const uniqPonketmons = [...new Set(nums)]
     
-    const ponketmonTypesCount = Object.keys(ponketmonMap).length
-    
-    return n > ponketmonTypesCount ? ponketmonTypesCount : n;
+    return n > uniqPonketmons.length ? uniqPonketmons.length : n;
 }
