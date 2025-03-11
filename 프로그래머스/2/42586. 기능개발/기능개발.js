5ae5d4eda12176b9ecc39831507f9baab15039a9
Seoyoung Hwang / Seo <emma05127@gmail.com>
2025-03-11 21:29:30 +0900

    [level 2] Title: 기능개발, Time: 0.33 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,24 +1,25 @@
 function solution(progresses, speeds) {
-    var answer = [];
-    let i=0;
+    const answer = [];
+    
     while(progresses.length){
-        if(progresses[i] < 100) progresses[i] += speeds[i];
-        i++;
+        // 개발 속도 별로 작업 진행
+        for(let i=0; i<progresses.length; i++){
+            progresses[i] += speeds[i];
+        }
         
-        if(i >= progresses.length) {
-            i=0;
-            let cnt = 0;
-            while(progresses[0] >= 100) {
-                progresses.shift();
-                speeds.shift();
-                cnt++;
-            }
-            if(cnt > 0) answer.push(cnt)
-        };
+        // 선 배포 작업 순서대로 완료된 작업 배포
+        let deployedWorkCount = 0;
+        while(progresses[0] >= 100){
+            progresses.shift();
+            speeds.shift();
+            deployedWorkCount +=1;
+        }
+        
+        // 만약 배포된 작업이 있다면 답변에 추가
+        if(deployedWorkCount) {
+            answer.push(deployedWorkCount)
+        }
     }
+    
     return answer;
 }
-
-// 1. progresses 배열을 순회하며 각 요소와 해당 요소에 idx에 해당하는 speeds 요소를 더한다.
-// 2. idx가 0이고 해당 요소가 100일 경우, 앞에서부터 요소의 값이 100 이상인 값들을 제거한다.
-// 3. 제거 중 100 미만 요소가 있을 경우 멈추고, 다시 1로 돌아간다.
