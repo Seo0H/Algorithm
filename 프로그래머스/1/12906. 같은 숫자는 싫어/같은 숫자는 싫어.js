b6bf8965de81a5bd88c13e98c9c21f3036cb6021
Seoyoung Hwang / Seo <emma05127@gmail.com>
2025-03-11 13:05:29 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 44.25 ms, Memory: 83 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,13 +1,3 @@
 function solution(arr) {
-    let lastNum = -1;
-    
-    
-    return arr.reduce((acc,cur) => {
-        if(lastNum === cur) return acc
-        
-        // 연속된 중복 값이 아닌 경우
-        acc.push(cur);
-        lastNum = cur;
-        return acc
-    } ,[]);
+    return arr.filter((cur,index) => cur !== arr[index+1])
 }
