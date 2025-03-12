a7aa5e0a186be63c7b6715c2a2699d6953be5bc9
Seoyoung Hwang / Seo <emma05127@gmail.com>
2025-03-12 20:14:23 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 15.00 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
@@ -1,26 +1,26 @@
 function solution(bridge_length, weight, truck_weights) {
-    var count = 1;
-    const q = [];
+    let count = 1;
+    const crossingTrucks = []; // { crossingTrucks , truck }[]
+    let onBridgeTrucksWeight = 0;
     
-    while(truck_weights.length || q.length){
-        
-        let sumQ = q.length ? q.reduce((acc,cur) => acc + cur.trukWeight, 0) : 0;
-        
-        if(q[0] && q[0].time === bridge_length) q.shift();
-        
-        if(sumQ + truck_weights[0] <= weight){
-            sumQ += truck_weights[0]
-            q.push({trukWeight : truck_weights.shift(), time:1});
+    while(truck_weights.length || crossingTrucks.length ){
+        // 다리에 대기중인 트럭이 올라갈 수 있는 경우, 다리에 트럭을 올린다.
+        if(onBridgeTrucksWeight + truck_weights[0] <= weight){
+            const curTruck =  truck_weights.shift();
+            crossingTrucks.push({ onBridgeTime : 1 , truck: curTruck });
+            onBridgeTrucksWeight += curTruck;
         }
         
-        q.forEach(el => el.time++);
-        count++;      
+        // 다리 위에 트럭이 있고, 트럭이 다리 위에 있었던 시간이 다리 길이와 같으면 다리를 지났다고 간주한다.
+        if(crossingTrucks.length && crossingTrucks[0].onBridgeTime === bridge_length) {
+            onBridgeTrucksWeight -= crossingTrucks.shift().truck;
+        }
         
+        // 다리에 있는 트럭의 시간을 추가한다.
+        crossingTrucks.forEach(truck => truck.onBridgeTime++)
+        
+        count += 1;
     }
     
     return count;
 }
-
-// 다리를 건널 시 다리 길이 시간만큼 해당 다리에 위치해 있어야 함.
-// 경과 시간과 truck이 지나가는건 따로따로 흘러감.
-// 직관적으로 풀어보기
