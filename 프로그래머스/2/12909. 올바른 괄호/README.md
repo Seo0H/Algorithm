5c540c71f41a57e76f13c47c6698b2d2e260f862
Seoyoung Hwang / Seo <emma05127@gmail.com>
2025-03-12 18:04:35 +0900

    [level 2] Title: 올바른 괄호, Time: 26.69 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/README.md
+++ 프로그래머스/2/12909. 올바른 괄호/README.md
@@ -1,10 +1,10 @@
 # [level 2] 올바른 괄호 - 12909 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909#) 
 
 ### 성능 요약
 
-메모리: 37.2 MB, 시간: 29.89 ms
+메모리: 36.6 MB, 시간: 26.69 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 12일 18:02:01
+2025년 03월 12일 18:04:33
 
 ### 문제 설명
 
--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,31 +1,11 @@
-const OPEN = '(';
-const CLOSE = ')';
-
 function solution(s){
-    const q = [];
+    if( s[s.length-1] === '(' || s[0] === ')' ) return false;
     
-    if( s[s.length-1] === OPEN || s[0] === CLOSE ) return false;
-    
+    let count = 0;
     for(let i=0; i<s.length; i++){
-        // 현재 문자열이 열려있으면
-        if(s[i] === OPEN){
-            // 큐에 ( 를 추가한다.
-            q.push(OPEN);
-            continue;
-        }
-        
-        // 현재 문자열이 닫혀있으면
-        // 큐에 마지막 요소가 열려있는지 확인한다
-        if(q[q.length-1] === OPEN) {
-            // 마지막 요소가 열려있으면 올바른 괄호로 처리한다
-            q.pop();
-            continue;
-        }
-        
-        // 현재 문자열이 닫혀있고, 큐의 마지막 요소가 닫혀있는 경우
-        return false
-        
+        count += s[i] === '(' ? 1 : -1 ;
+        if(count < 0) return false
     }
 
-    return !q.length;
+    return count === 0
 }
