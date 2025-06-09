9470b61d5337e3de6838132fbfbd5a6a4ff83ff6
Seoyoung Hwang / Seo <emma05127@gmail.com>
2025-06-09 17:52:32 +0900

    [level 3] Title: 숫자 게임, Time: 83.35 ms, Memory: 43.7 MB -BaekjoonHub

--- 프로그래머스/3/12987. 숫자 게임/숫자 게임.js
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.js
@@ -1,15 +1,14 @@
 function solution(A, B) {
     let answer = 0;
-    const check = new Set();
     let idx = A.length - 1;
     
     A.sort((a,b) => a - b);
     B.sort((a,b) => a - b);
     
-    
     while(idx >= 0) {
         const a = A[idx];
         const b = B[idx];
+        
         idx -= 1;
         
        if(a < b) {
