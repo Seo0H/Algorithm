dadeae3f115241851315160fe81b569650974526
Seoyoung Hwang / Seo <emma05127@gmail.com>
2025-06-17 19:26:45 +0900

    [level 1] Title: 모의고사, Time: 4.15 ms, Memory: 37.6 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,48 +1,37 @@
-
-const patterns = {
-  1: [1, 2, 3, 4, 5],
-  2: [2, 1, 2, 3, 2, 4, 2, 5],
-  3: [3, 3, 1, 1, 2, 2, 4, 4, 5, 5],
-};
-
 function solution(answers) {
-  const results = getResults(answers);
-  const max = Math.max(...results);
-  return results.reduce((acc,cur,no) => {
-      if(cur === max) acc.push(no);
-      return acc
-  }, []).sort()
+    if(!answers.length) return []
+    
+    // user info list
+    const users = [
+        {
+            id: 1,
+            answer : [1, 2, 3, 4, 5],
+            count : 0
+        },
+        {
+            id: 2,
+            answer : [2, 1, 2, 3, 2, 4, 2, 5],
+            count : 0
+        },
+        {
+            id: 3,
+            answer : [3, 3, 1, 1, 2, 2, 4, 4, 5, 5],
+            count : 0
+        }
+    ];
+    
+    // calculation count
+    answers.forEach((answer, idx) => {
+        for(const user of users) {
+            if(user.answer[idx % user.answer.length] === answer) {
+                user.count += 1;
+            }
+        }
+    })
+    
+    // check max count
+    const maxCount = Math.max(...users.map(({ count }) => count));
+    
+    // find max count users
+    return users.filter(({count}) => count === maxCount).map(({id}) => id) 
 }
-
-/**
- * @param {number[]} answers
- * @returns {[null, number, number, number]} results 수포자 번호가 index이고 각 요소의 값은 점수
- */
-const getResults = (answers) => {
-  const noIdxMap = { 1: 0, 2: 0, 3: 0 };
-
-  return answers.reduce(
-    (memo, testAnswer) => {
-      for (let no in noIdxMap) {
-        // 현재 수포자의 패턴
-        const abandonerPattern = patterns[no];
-
-        // idx 검증
-        if (noIdxMap[no] >= abandonerPattern.length) {
-          noIdxMap[no] = 0;
-        }
-
-        const curIdx = noIdxMap[no];
-        noIdxMap[no] += 1;
-
-        // 정답 평가
-        if (abandonerPattern[curIdx] === testAnswer) {
-          memo[no] += 1;
-        }
-      }
-
-      return memo;
-    },
-    [null, 0, 0, 0]
-  );
-};
