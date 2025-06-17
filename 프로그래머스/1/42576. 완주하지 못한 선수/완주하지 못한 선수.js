a06299e42064582e9feda3b007604eeb0721e613
Seoyoung Hwang / Seo <emma05127@gmail.com>
2025-06-17 17:59:38 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 74.29 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,25 +1,16 @@
 function solution(participant, completion) {
-    // 완료자 체크 맵, 동명이인일 경우를 위해 완료자 횟수로 체크
-    const completionMap = completion.reduce((acc,cur) => {
-        if(acc[cur]){
-            acc[cur] += 1;
-        } else {
-            acc[cur] = 1;
-        }
-        
-        return acc
-    }, {})
+    const completionMap = completion.reduce((nameMap,cur) => {
+        nameMap.set(cur, nameMap.has(cur) ? nameMap.get(cur) + 1 : 1 )
+        return nameMap
+    }, new Map());
 
-    for(let i=0; i < participant.length ; i++){
-        // 현재 참가자
-        const curParticipant = participant[i];
-        
-        // 만약 현재 참가자가 완료 명단에 있을 경우,
-        if(completionMap[curParticipant]) {
-            completionMap[curParticipant] -= 1;
-            continue;
+    for(const participantName of participant){
+        if(completionMap.has(participantName) === false) {
+            return participantName
         }
         
-        return curParticipant
+        completionMap.set(participantName, completionMap.get(participantName) - 1)
     }
+    
+    return [...completionMap].filter(([name, count]) => count < 0).at(0).at(0)
 }
